940c4d56a625a5f206d13785691df86a734402c4
ilya osin <osin1@mail.ru>
2024-05-28 23:31:29 +0300

    fix: исправлены ошибки, добавлен более детальный alarm

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -9,7 +9,5 @@ import io.ktor.server.netty.*
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
-
     }.start(wait = true)
-
 }
--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -16,31 +16,68 @@ fun Application.configureRouting() {
         gson()
     }
     routing {
-        staticResources(remotePath = "/", basePackage = "static", index="index.html")
+        staticResources(remotePath = "/", basePackage = "static", index = "index.html")
 
-        post("/calculate"){
+        post("/calculate") {
             val stringFromJson = call.receive<MatrixString>().value
 
-            if (stringFromJson.length<=5){
+            if (stringFromJson.length <= 5) {
                 val err = "Введите обе матрицы!"
                 call.respondText(err, contentType = ContentType.Text.Plain, status = HttpStatusCode(418, "math error"))
-            } else{
+            } else {
                 val matrixPair = stringFromJson.split("|")
                 val matrixLeft = matrixPair[0]
                 val matrixRight = matrixPair[1]
-                if  (matrixLeft.length<3 || matrixRight.length<3){
+                if (matrixLeft.length < 3 || matrixRight.length < 3) {
                     val err = "Одна из матриц пуста"
-                    call.respondText(err, contentType = ContentType.Text.Plain, status = HttpStatusCode(418, "math error"))
-                } else{
-                    val m1:Matrix = parseStringToMatrix(matrixLeft)!!
-                    val m2:Matrix = parseStringToMatrix(matrixRight)!!
+                    call.respondText(
+                        err,
+                        contentType = ContentType.Text.Plain,
+                        status = HttpStatusCode(418, "math error")
+                    )
+                } else {
+                    val matrixLeftPair  = parseStringToMatrix(matrixLeft)
+                    val matrixRightPair  = parseStringToMatrix(matrixRight)
+                    val m1 = matrixLeftPair.first
+                    val m2 = matrixRightPair.first
+                    val errorL = matrixLeftPair.second
+                    val errorR = matrixRightPair.second
 
-                    val matProd = matrixProduct(m1, m2)
-                    if (matProd.isNullOrEmpty()){
-                        val err = "Число столбцов первой матрицы должно равняться числу строк второй!"
-                        call.respondText(err, contentType = ContentType.Text.Plain, status = HttpStatusCode(418, "math error"))
-                    } else{
-                        call.respond<Matrix>(matProd)
+                    if (errorL.isNotEmpty()){
+                        call.respondText(
+                            errorL,
+                            contentType = ContentType.Text.Plain,
+                            status = HttpStatusCode(418, "parse error")
+                        )
+                    }
+
+                    if (errorR.isNotEmpty()){
+                        call.respondText(
+                            errorR,
+                            contentType = ContentType.Text.Plain,
+                            status = HttpStatusCode(418, "parse error")
+                        )
+                    }
+
+                    if (m1.isNullOrEmpty() || m2.isNullOrEmpty()){
+                        val err = "Одна из матриц не заполнена"
+                        call.respondText(
+                            err,
+                            contentType = ContentType.Text.Plain,
+                            status = HttpStatusCode(418, "math error")
+                        )
+                    } else {
+                        val matProd = matrixProduct(m1, m2)
+                        if (matProd.isNullOrEmpty()) {
+                            val err = "Число столбцов первой матрицы должно равняться числу строк второй!"
+                            call.respondText(
+                                err,
+                                contentType = ContentType.Text.Plain,
+                                status = HttpStatusCode(418, "math error")
+                            )
+                        } else {
+                            call.respond<Matrix>(matProd)
+                        }
                     }
                 }
             }
